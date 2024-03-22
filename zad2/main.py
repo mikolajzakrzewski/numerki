959432b0c72d24e9b8ce08a4b89669d6aa30e812
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-03-22 19:20:52 +0100

    Added 4x4 matrix determinant calculation method and Jacobian Method iteration method

--- zad2/main.py
+++ zad2/main.py
@@ -18,7 +18,7 @@ def file_read(filename):
                       range(0, len(matrix_line), num_unknowns)]
 
             vector = list(map(float, file.readline().strip().split()))
-
+            matrix = np.array(matrix)
             return num_unknowns, matrix, vector
 
     except FileNotFoundError:
@@ -80,6 +80,19 @@ def get_3x3_determinant(matrix):
     return determinant
 
 
+def get_4x4_determinant(matrix):
+    determinant = 0
+    for i in range(len(matrix)):
+        multiplier = matrix[0][i]
+        reduced_matrix = np.delete(np.delete(matrix, 0, 0), i, 1)
+        if i % 2 == 2:
+            determinant += multiplier * get_3x3_determinant(reduced_matrix)
+        else:
+            determinant -= multiplier * get_3x3_determinant(reduced_matrix)
+
+    return determinant
+
+
 def get_adjoint_matrix(matrix):
     cofactors_matrix = get_cofactors_matrix(matrix)
     adjoint_matrix = np.transpose(cofactors_matrix)
@@ -89,7 +102,12 @@ def get_adjoint_matrix(matrix):
 def get_inverse_matrix(matrix):
     inverse_matrix = np.zeros((len(matrix), len(matrix)))
     adjoint_matrix = get_adjoint_matrix(matrix)
-    det = det_matrix(matrix, len(matrix))
+    if len(matrix) == 2:
+        det = get_2x2_determinant(matrix)
+    elif len(matrix) == 3:
+        det = get_3x3_determinant(matrix)
+    elif len(matrix) == 4:
+        det = get_4x4_determinant(matrix)
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             inverse_matrix[i][j] = adjoint_matrix[i][j] / det
@@ -121,6 +139,36 @@ def get_upper_triangular_matrix(matrix):
             upper_triangular_matrix[i][j] = matrix[i][j]
 
     return upper_triangular_matrix
+
+
+def get_t_matrix(matrix):
+    inverse_diagonal_matrix = get_inverse_matrix(get_diagonal_matrix(matrix))
+    lower_triangular_matrix = get_lower_triangular_matrix(matrix)
+    upper_triangular_matrix = get_upper_triangular_matrix(matrix)
+    lower_triangular_matrix_plus_upper_triangular_matrix = np.zeros((len(matrix), len(matrix)))
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            lower_triangular_matrix_plus_upper_triangular_matrix[i][j] = (
+                    lower_triangular_matrix[i][j] + upper_triangular_matrix[i][j]
+            )
+    t_matrix = np.matmul(inverse_diagonal_matrix, lower_triangular_matrix_plus_upper_triangular_matrix)
+    return t_matrix
+
+
+def get_c_matrix(matrix, b):
+    inverse_diagonal_matrix = get_inverse_matrix(matrix)
+    c_matrix = np.matmul(inverse_diagonal_matrix, b)
+    return c_matrix
+
+
+def jacobian_iteration(matrix, b, x):
+    t_matrix = get_t_matrix(matrix)
+    c_matrix = get_c_matrix(matrix, b)
+    t_and_x_multiplied = np.matmul(t_matrix, x)
+    new_x = np.zeros((len(x), 0))
+    for i in range(len(x)):
+        new_x[i][0] = t_and_x_multiplied[i][0] + c_matrix[i][0]
+    return new_x
 
 
 def main():
@@ -159,8 +207,8 @@ def main():
         upper_triangular_matrix = get_upper_triangular_matrix(matrix)
         display_matrix(upper_triangular_matrix)
 
-        determinant = det_matrix(matrix, num_unknowns)
-        print("Wyznacznik macierzy A:", determinant)
+        # determinant = det_matrix(matrix, num_unknowns)
+        # print("Wyznacznik macierzy A:", determinant)
 
         cofactors_matrix = get_cofactors_matrix(matrix)
         print("Macierz wyznaczników: ")
@@ -171,6 +219,10 @@ def main():
         inverse_matrix = get_inverse_matrix(matrix)
         print("Macierz odwrotna: ")
         print(inverse_matrix)
+
+        t_matrix = get_t_matrix(matrix)
+        print("Macierz T:")
+        print(t_matrix)
 
         cont = input("Czy chcesz kontynuować? (T/N): ")
         if cont.upper() != 'T':
