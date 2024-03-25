36be78843d5169e92f417ea08aecf12739b3f587
Mikołaj Zakrzewski <mikizakr@gmail.com>
2024-03-25 20:35:27 +0100

    Moved matrix operations implementation methods from main.py to a separate file

--- zad2/main.py
+++ zad2/main.py
@@ -1,11 +1,7 @@
 import os
 
 import numpy as np
-
-
-def display_matrix(matrix):
-    for row in matrix:
-        print(row)
+import matrix_operations as mo
 
 
 def file_read(filename):
@@ -30,144 +26,9 @@ def file_read(filename):
     #     return None
 
 
-def det_matrix(matrix, n):
-    what_return = 0
-    if n == 1:
-        return matrix[0][0]
-    else:
-        for i in range(n):
-            pomocnicza = [row[:i] + row[i + 1:] for row in matrix[1:]]
-            if i % 2 == 0:
-                what_return += matrix[0][i] * det_matrix(pomocnicza, n - 1)
-            else:
-                what_return -= matrix[0][i] * det_matrix(pomocnicza, n - 1)
-        return what_return
-
-
-def get_cofactors_matrix(matrix):
-    cofactors_matrix = np.zeros((len(matrix), len(matrix)))
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            multiplier = -1
-            for k in range(i + j + 1):
-                multiplier *= -1
-
-            reduced_matrix = np.delete(np.delete(matrix, i, 0), j, 1)
-            if len(reduced_matrix) == 2:
-                cofactor = multiplier * get_2x2_determinant(reduced_matrix)
-                cofactors_matrix[i][j] = cofactor
-            elif len(reduced_matrix) == 3:
-                cofactor = multiplier * get_3x3_determinant(reduced_matrix)
-                cofactors_matrix[i][j] = cofactor
-
-    return cofactors_matrix
-
-
-def get_2x2_determinant(matrix):
-    determinant = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    return determinant
-
-
-def get_3x3_determinant(matrix):
-    determinant = 0
-    for i in range(len(matrix)):
-        multiplier = matrix[0][i]
-        reduced_matrix = np.delete(np.delete(matrix, 0, 0), i, 1)
-        if i % 2 == 0:
-            determinant += multiplier * get_2x2_determinant(reduced_matrix)
-        else:
-            determinant -= multiplier * get_2x2_determinant(reduced_matrix)
-
-    return determinant
-
-
-def get_4x4_determinant(matrix):
-    determinant = 0
-    for i in range(len(matrix)):
-        multiplier = matrix[0][i]
-        reduced_matrix = np.delete(np.delete(matrix, 0, 0), i, 1)
-        if i % 2 == 2:
-            determinant += multiplier * get_3x3_determinant(reduced_matrix)
-        else:
-            determinant -= multiplier * get_3x3_determinant(reduced_matrix)
-
-    return determinant
-
-
-def get_adjoint_matrix(matrix):
-    cofactors_matrix = get_cofactors_matrix(matrix)
-    adjoint_matrix = np.transpose(cofactors_matrix)
-    return adjoint_matrix
-
-
-def get_inverse_matrix(matrix):
-    inverse_matrix = np.zeros((len(matrix), len(matrix)))
-    adjoint_matrix = get_adjoint_matrix(matrix)
-    if len(matrix) == 2:
-        det = get_2x2_determinant(matrix)
-    elif len(matrix) == 3:
-        det = get_3x3_determinant(matrix)
-    elif len(matrix) == 4:
-        det = get_4x4_determinant(matrix)
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            inverse_matrix[i][j] = adjoint_matrix[i][j] / det
-
-    return inverse_matrix
-
-
-def get_diagonal_matrix(matrix):
-    diagonal_matrix = np.zeros((len(matrix), len(matrix)))
-    for i in range(len(matrix)):
-        diagonal_matrix[i][i] = matrix[i][i]
-
-    return diagonal_matrix
-
-
-def get_lower_triangular_matrix(matrix):
-    lower_triangular_matrix = np.zeros((len(matrix), len(matrix)))
-    for i in range(1, len(matrix)):
-        for j in range(0, i):
-            lower_triangular_matrix[i][j] = matrix[i][j]
-
-    return lower_triangular_matrix
-
-
-def get_upper_triangular_matrix(matrix):
-    upper_triangular_matrix = np.zeros((len(matrix), len(matrix)))
-    for i in range(len(matrix) - 1):
-        for j in range(1 + i, len(matrix)):
-            upper_triangular_matrix[i][j] = matrix[i][j]
-
-    return upper_triangular_matrix
-
-
-def get_t_matrix(matrix):
-    inverse_diagonal_matrix = get_inverse_matrix(get_diagonal_matrix(matrix))
-    for i in range(len(inverse_diagonal_matrix)):
-        for j in range(len(inverse_diagonal_matrix)):
-            inverse_diagonal_matrix[i][j] *= -1
-    lower_triangular_matrix = get_lower_triangular_matrix(matrix)
-    upper_triangular_matrix = get_upper_triangular_matrix(matrix)
-    lower_triangular_matrix_plus_upper_triangular_matrix = np.zeros((len(matrix), len(matrix)))
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            lower_triangular_matrix_plus_upper_triangular_matrix[i][j] = (
-                    lower_triangular_matrix[i][j] + upper_triangular_matrix[i][j]
-            )
-    t_matrix = np.matmul(inverse_diagonal_matrix, lower_triangular_matrix_plus_upper_triangular_matrix)
-    return t_matrix
-
-
-def get_c_matrix(matrix, b):
-    inverse_diagonal_matrix = get_inverse_matrix(get_diagonal_matrix(matrix))
-    c_matrix = np.matmul(inverse_diagonal_matrix, b)
-    return c_matrix
-
-
 def jacobian_iteration(matrix, b, x):
-    t_matrix = get_t_matrix(matrix)
-    c_matrix = get_c_matrix(matrix, b)
+    t_matrix = mo.get_t_matrix(matrix)
+    c_matrix = mo.get_c_matrix(matrix, b)
     t_and_x_multiplied = np.matmul(t_matrix, x)
     new_x = np.zeros((len(x), 1))
     for i in range(len(x)):
@@ -197,34 +58,34 @@ def main():
         print("Wymiar macierzy n: ", num_unknowns)
 
         print("Macierz A: ")
-        display_matrix(matrix)
+        mo.display_matrix(matrix)
 
         print("Macierz D")
-        diagonal_matrix = get_diagonal_matrix(matrix)
-        display_matrix(diagonal_matrix)
+        diagonal_matrix = mo.get_diagonal_matrix(matrix)
+        mo.display_matrix(diagonal_matrix)
 
         print("Macierz L")
-        lower_triangular_matrix = get_lower_triangular_matrix(matrix)
-        display_matrix(lower_triangular_matrix)
+        lower_triangular_matrix = mo.get_lower_triangular_matrix(matrix)
+        mo.display_matrix(lower_triangular_matrix)
 
         print("Macierz U")
-        upper_triangular_matrix = get_upper_triangular_matrix(matrix)
-        display_matrix(upper_triangular_matrix)
+        upper_triangular_matrix = mo.get_upper_triangular_matrix(matrix)
+        mo.display_matrix(upper_triangular_matrix)
 
-        # determinant = det_matrix(matrix, num_unknowns)
-        # print("Wyznacznik macierzy A:", determinant)
+        det = mo.get_determinant(matrix)
+        print("Wyznacznik macierzy A:", det)
 
-        cofactors_matrix = get_cofactors_matrix(matrix)
+        cofactors_matrix = mo.get_cofactors_matrix(matrix)
         print("Macierz wyznaczników: ")
         print(cofactors_matrix)
         print("Adjoint matrix: ")
-        print(get_adjoint_matrix(cofactors_matrix))
+        print(mo.get_adjoint_matrix(cofactors_matrix))
 
-        inverse_matrix = get_inverse_matrix(matrix)
+        inverse_matrix = mo.get_inverse_matrix(matrix)
         print("Macierz odwrotna: ")
         print(inverse_matrix)
 
-        t_matrix = get_t_matrix(matrix)
+        t_matrix = mo.get_t_matrix(matrix)
         print("Macierz T:")
         print(t_matrix)
 
---
+++ zad2/matrix_operations.py
@@ -0,0 +1,103 @@
+import numpy as np
+
+
+def display_matrix(matrix):
+    for row in matrix:
+        print(row)
+
+
+def get_determinant(matrix):
+    determinant = 0
+    if len(matrix) == 1:
+        return matrix[0][0]
+    else:
+        for i in range(len(matrix)):
+            multiplier = matrix[0][i]
+            reduced_matrix = np.delete(np.delete(matrix, 0, 0), i, 1)
+            if i % 2 == 0:
+                determinant += multiplier * get_determinant(reduced_matrix)
+            else:
+                determinant -= multiplier * get_determinant(reduced_matrix)
+
+    return determinant
+
+
+def get_cofactors_matrix(matrix):
+    cofactors_matrix = np.zeros((len(matrix), len(matrix)))
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            multiplier = -1
+            for k in range(i + j + 1):
+                multiplier *= -1
+
+            reduced_matrix = np.delete(np.delete(matrix, i, 0), j, 1)
+            cofactor = multiplier * get_determinant(reduced_matrix)
+            cofactors_matrix[i][j] = cofactor
+
+    return cofactors_matrix
+
+
+def get_adjoint_matrix(matrix):
+    cofactors_matrix = get_cofactors_matrix(matrix)
+    adjoint_matrix = np.transpose(cofactors_matrix)
+    return adjoint_matrix
+
+
+def get_inverse_matrix(matrix):
+    inverse_matrix = np.zeros((len(matrix), len(matrix)))
+    adjoint_matrix = get_adjoint_matrix(matrix)
+    det = get_determinant(matrix)
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            inverse_matrix[i][j] = adjoint_matrix[i][j] / det
+
+    return inverse_matrix
+
+
+def get_diagonal_matrix(matrix):
+    diagonal_matrix = np.zeros((len(matrix), len(matrix)))
+    for i in range(len(matrix)):
+        diagonal_matrix[i][i] = matrix[i][i]
+
+    return diagonal_matrix
+
+
+def get_lower_triangular_matrix(matrix):
+    lower_triangular_matrix = np.zeros((len(matrix), len(matrix)))
+    for i in range(1, len(matrix)):
+        for j in range(0, i):
+            lower_triangular_matrix[i][j] = matrix[i][j]
+
+    return lower_triangular_matrix
+
+
+def get_upper_triangular_matrix(matrix):
+    upper_triangular_matrix = np.zeros((len(matrix), len(matrix)))
+    for i in range(len(matrix) - 1):
+        for j in range(1 + i, len(matrix)):
+            upper_triangular_matrix[i][j] = matrix[i][j]
+
+    return upper_triangular_matrix
+
+
+def get_t_matrix(matrix):
+    inverse_diagonal_matrix = get_inverse_matrix(get_diagonal_matrix(matrix))
+    for i in range(len(inverse_diagonal_matrix)):
+        for j in range(len(inverse_diagonal_matrix)):
+            inverse_diagonal_matrix[i][j] *= -1
+    lower_triangular_matrix = get_lower_triangular_matrix(matrix)
+    upper_triangular_matrix = get_upper_triangular_matrix(matrix)
+    lower_triangular_matrix_plus_upper_triangular_matrix = np.zeros((len(matrix), len(matrix)))
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            lower_triangular_matrix_plus_upper_triangular_matrix[i][j] = (
+                    lower_triangular_matrix[i][j] + upper_triangular_matrix[i][j]
+            )
+    t_matrix = np.matmul(inverse_diagonal_matrix, lower_triangular_matrix_plus_upper_triangular_matrix)
+    return t_matrix
+
+
+def get_c_matrix(matrix, b):
+    inverse_diagonal_matrix = get_inverse_matrix(get_diagonal_matrix(matrix))
+    c_matrix = np.matmul(inverse_diagonal_matrix, b)
+    return c_matrix
