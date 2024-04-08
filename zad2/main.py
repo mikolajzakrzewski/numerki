dc89d137e50c9e837fa4904cd806430fce6f5127
247720 Piotr Lewkowicz <247720@edu.p.lodz.pl>
2024-04-08 23:57:16 +0200

    Added manual matrix input

--- zad2/main.py
+++ zad2/main.py
@@ -3,8 +3,6 @@ import os
 import numpy as np
 
 import jacobi_method
-import matrix_operations as mo
-import jacobi_method as jm
 
 
 def file_read(filename):
@@ -21,26 +19,45 @@ def file_read(filename):
         return num_unknowns, matrix, vector
 
 
+def manual_matrix_input():
+    print("Wprowadź wymiar macierzy:")
+    num_unknowns = int(input())
+
+    print("Wprowadź wartości elementów macierzy:")
+    matrix_input = [float(x) for x in input().split()]
+
+    matrix = [matrix_input[i:i + num_unknowns] for i in range(0, len(matrix_input), num_unknowns)]
+    vector = [float(x) for x in input("Wprowadź wektor wynikowy oddzielony spacjami: ").split()]
+
+    return num_unknowns, np.array(matrix), np.array(vector)
+
 def main():
     while True:
         print('--------------------Program do rozwiazywania układu N równań liniowych z N niewiadomymi '
               'metodą iteracyjną Jacobiego--------------------')
 
-        filename = input("wybierz plik z układem równań - [a,b,c,d,e,f,g,h,i,j]: ") + ".txt"
-        print("wybrany plik:", filename)
+        print('Wybierz opcję:')
+        print('1. Wybierz plik z układem równań')
+        print('2. Wprowadź własną macierz')
+        choice = input()
 
-        if not os.path.exists(filename):
-            print("plik nie istnieje, wpisz jeszcze raz")
+        if choice == '1':
+            filename = input("Wybierz plik z układem równań - [a,b,c,d,e,f,g,h,i,j]: ") + ".txt"
+            print("Wybrany plik:", filename)
+            if not os.path.exists(filename):
+                print("Plik nie istnieje, wpisz jeszcze raz")
+                continue
+            data = file_read(filename)
+            num_unknowns, matrix, vector = data
+            x = np.zeros(len(vector))
+
+        elif choice == '2':
+            data = manual_matrix_input()
+            num_unknowns, matrix, vector = data
+            x = np.zeros(len(vector))
+        else:
+            print('Niepoprawny wybór. Wybierz jeszcze raz.')
             continue
-
-        data = file_read(filename)
-
-        if data is None:
-            continue
-
-        num_unknowns, matrix, vector = data
-
-        x = np.zeros(len(vector))
 
         print('Wybierz warunek stopu:\n1. Zadana dokładność\n2. Metoda iteracyjna')
         while True:
